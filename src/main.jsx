4232d62a050b5c747eb4701f3658398787a18ecf
xavier <samuelpedroso08@gmail.com>
2024-09-16 15:50:25 -0300

    subindo arquivos

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Projeto para Fazer a Prática</h1>
+  </StrictMode>,
+)
